8e8dac198d78b510c5255d28dcb3098cc968667d
Gavriel Dabbah <gavriel3@gmail.com>
2022-02-03 04:25:12 +0200

    Update script_on_container.sh

--- script_on_container.sh
+++ script_on_container.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 X=$(docker ps | grep alpcon)
-if [[$X==""]];then
+if [[ -z $X ]];then
     docker start alpcon
 fi
-docker exec -d alpcon wget https://raw.githubusercontent.com/gavrield/DockerProject/main/print_and_wait.py
-docker exec -d alpcon chmod 777 print_and_wait.py
+docker exec -d alpcon wget https://raw.githubusercontent.com/gavrield/DockerProject/main/print_and_wait.py &
+wait
+docker exec -d alpcon chmod 711 print_and_wait.py &
+wait
 docker exec -d alpcon ./print_and_wait.py
 sleep 10
-sudo kill $(docker top alpcon | grep python3 | grep -v grep | awk '{print $2}')
+echo 123 | sudo kill $(docker top alpcon | grep python3 | grep -v grep | awk '{print $2}')
 docker stop alpcon
