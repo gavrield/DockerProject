2706c9d7f6769b171286c10419abbb358352e3f2
Gavriel Dabbah <gavriel3@gmail.com>
2022-02-06 02:03:47 +0200

    Update script_on_container.sh

--- script_on_container.sh
+++ script_on_container.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
-X=$(docker ps | grep alpcon)
-if [[ -z $X ]];then
-    docker start alpcon
+X=$(docker ps | grep alpcon);
+if [[ -z $X ]]; then
+    docker start alpcon;
 fi
-docker exec -d alpcon wget https://raw.githubusercontent.com/gavrield/DockerProject/main/print_and_wait.py &
-wait
-docker exec -d alpcon chmod 711 print_and_wait.py &
-wait
-docker exec -d alpcon ./print_and_wait.py
-sleep 10
-echo 123 | sudo kill $(docker top alpcon | grep python3 | grep -v grep | awk '{print $2}')
-docker stop alpcon
+docker exec -d alpcon wget https://raw.githubusercontent.com/gavrield/DockerProject/main/print_and_wait.py;
+docker exec -d alpcon chmod 711 print_and_wait.py;
+docker exec -d alpcon ./print_and_wait.py;
+sleep 10;
+PID=$(docker top alpcon | grep python3 | grep -v grep | awk '{print $2}');
+if [[ -z $PID]]; then
+    echo 123 | sudo kill;
+    echo "failed test";
+fi
+docker stop alpcon;
