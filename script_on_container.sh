9c85a8a9b51956c1a0457f573d72312363896f3a
Gavriel Dabbah <gavriel3@gmail.com>
2022-02-08 15:59:59 +0200

    Update script_on_container.sh

--- script_on_container.sh
+++ script_on_container.sh
@@ -8,8 +8,8 @@ docker exec -d alpcon chmod 711 print_and_wait.py;
 docker exec -d alpcon ./print_and_wait.py;
 sleep 10;
 PID=$(docker top alpcon | grep python3 | grep -v grep | awk '{print $2}');
-if [[ -z $PID]]; then
-    echo 123 | sudo kill;
+if [[ ! -z $PID ]]; then
+    echo 123 | sudo kill ${PID};
     echo "failed test";
 fi
 docker stop alpcon;
