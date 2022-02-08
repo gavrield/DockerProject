1bd83b0ab20f3bf46387182cc009f6236ab10e28
Gavriel Dabbah <gavriel3@gmail.com>
2022-02-08 16:17:25 +0200

    Update script_on_container.sh

--- script_on_container.sh
+++ script_on_container.sh
@@ -4,8 +4,7 @@ if [[ -z $X ]]; then
     docker start alpcon;
 fi
 docker exec -d alpcon wget https://raw.githubusercontent.com/gavrield/DockerProject/main/print_and_wait.py;
-docker exec -d alpcon chmod 711 print_and_wait.py;
-docker exec -d alpcon ./print_and_wait.py;
+docker exec -d alpcon python3 ./print_and_wait.py;
 sleep 10;
 PID=$(docker top alpcon | grep python3 | grep -v grep | awk '{print $2}');
 if [[ ! -z $PID ]]; then
