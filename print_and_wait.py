32bf3cdf7cebf15a036e1aae385c8357a87473eb
Gavriel Dabbah <gavriel3@gmail.com>
2022-02-09 05:00:46 +0200

    Update print_and_wait.py

--- print_and_wait.py
+++ print_and_wait.py
@@ -3,5 +3,5 @@ import time
 i = 0
 while i < 100:
   i += 1
-  print(i,". Hi from program", file=open("/data/out.txt", "a"))
+  print(i,". Hi from long time program", file=open("/data/out.txt", "a"))
   time.sleep(2)
