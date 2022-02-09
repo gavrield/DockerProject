5241f52213f3639c45960fd0961e85cb02b77992
Gavriel Dabbah <gavriel3@gmail.com>
2022-02-09 04:55:19 +0200

    Update script_on_container.sh

--- script_on_container.sh
+++ script_on_container.sh
@@ -15,7 +15,7 @@ fi
 
 docker exec -d alpcon git clone https://github.com/gavrield/DockerProject.git;
 sleep 2;
-docker exec -d alpcon python3 /DockerProject/print_and_wait.py";
+docker exec -d alpcon python3 /DockerProject/print_and_wait.py;
 sleep 10;
 PID=$(docker top alpcon | grep python3 | awk '{print $2}');
 if [[ ! -z $PID ]]; then
