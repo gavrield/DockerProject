01e71dffc60c67982c65f9f006eea06b28ed0712
Gavriel Dabbah <gavriel3@gmail.com>
2022-02-09 05:01:23 +0200

    Update print_and_wait_stop.py

--- print_and_wait_stop.py
+++ print_and_wait_stop.py
@@ -3,6 +3,6 @@ import time
 i = 0
 while i < 2:
   i += 1
-  print(i,". Hi from program", file=open("/data/out.txt", "a"))
+  print(i,". Hi from short program", file=open("/data/out.txt", "a"))
   time.sleep(2)
   
